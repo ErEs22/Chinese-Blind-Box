9cf2d03f4e7ea74cf6b4a30300ee6fca8ca69af9
ErE <ere0506@qq.com>
2022-04-15 10:36:22 +0800

    项目初始文件提交

---
+++ Scripts/AutoDeactivate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// 
+/// </summary>
+public class AutoDeactivate : MonoBehaviour
+{
+    [SerializeField] float lifeTime = 3f;
+    [SerializeField] bool destroyGameObject;
+    WaitForSeconds waitLifeTime;
+    private void Awake()
+    {
+        waitLifeTime = new WaitForSeconds(lifeTime);
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(DeactivateCoroutine));
+    }
+    IEnumerator DeactivateCoroutine()
+    {
+        yield return waitLifeTime;
+        if (destroyGameObject)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            if(gameObject.activeSelf == true)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
